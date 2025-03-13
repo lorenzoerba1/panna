8e6e4987ffd3a1edfb89376b7ad34ac8fac5a170
Matteo Ceccarello <matteo.ceccarello@gmail.com>
2025-03-13 23:02:38 +0100

    so be it!

---
+++ Justfile
@@ -0,0 +1,20 @@
+test: build-tests
+    build/tests
+
+build-tests:
+    cmake --build build -j --config Debug --target tests
+
+clean:
+    cd build && make clean
+
+setup-cmake:
+    test -d build || mkdir build
+    cd build && cmake ..
+
+generate-compile-commands:
+    just clean
+    bear -- just build-tests
+
+scan-build:
+    just clean
+    scan-build just build
---
+++ flake.nix
@@ -0,0 +1,47 @@
+{
+  description = "PUFFINN - Parameterless and Universal Fast FInding of Nearest Neighbors";
+
+  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+
+  outputs = {
+    self,
+    nixpkgs,
+  }: let
+    supportedSystems = ["x86_64-linux" "aarch64-linux" "x86_64-darwin" "aarch64-darwin"];
+    forEachSupportedSystem = f:
+      nixpkgs.lib.genAttrs supportedSystems (system:
+        f {
+          pkgs = import nixpkgs {inherit system;};
+        });
+  in {
+    devShells = forEachSupportedSystem ({pkgs}: {
+      default =(pkgs.mkShell.override { stdenv = pkgs.clangStdenv; }) {
+        venvDir = ".venv";
+        
+        packages = with pkgs; [
+          clang-tools
+          clang-analyzer
+          lldb
+          python310
+          sqlite-interactive
+          cmake
+          just
+          bear # To generate compile_commands.json files
+          llvmPackages.openmp
+          llvmPackages.libcxx
+          rr
+          valgrind
+        ] ++ ( with python310Packages; [
+          setuptools
+          wheel
+          venvShellHook
+          numpy
+          h5py
+          pybind11
+        ]);
+
+        NIX_ENFORCE_NO_NATIVE = false;
+      };
+    });
+  };
+}
