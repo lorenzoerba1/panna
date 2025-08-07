9776de0cceb2a1686628fb3ab9780a016724e8ed
francescopio.monaco@studenti.unipd.it <francescopio.monaco@studenti.unipd.it>
2025-08-07 11:38:47 +0200

    Restore kd-tree dist heuristic as it works

--- include/panna/kdtree.hpp
+++ include/panna/kdtree.hpp
@@ -110,8 +110,21 @@ namespace panna {
 
         /// min squared distance between two bounding boxes
         float min_dist_sq_( size_t a_idx, size_t b_idx ) const {
-            return Distance::compute( dataset_[a_idx], dataset_[b_idx] );
+            float acc = 0.f;
+            const auto& a_node = nodes_[a_idx];
+            const auto& b_node = nodes_[b_idx];
+            for ( int d = 0; d < dim_; d++ ) {
+                float diff = 0.f;
+                if ( a_node.max[d] < b_node.min[d] )
+                    diff = b_node.min[d] - a_node.max[d];
+                else if ( b_node.max[d] < a_node.min[d] )
+                    diff = a_node.min[d] - b_node.max[d];
 
+                acc += diff * diff;
+                if ( acc > radius2_ )
+                    break; // Early exit
+            }
+            return acc;
         }
 
         // Enumerate pairs of points within the radius
@@ -122,7 +135,7 @@ namespace panna {
                     std::vector<std::tuple<float, std::pair<uint32_t, uint32_t>>>& out ) const {
             if ( a_idx == NO_NODE || b_idx == NO_NODE )
                 return;
-            if ( min_dist_sq_( a_idx, b_idx ) > radius_ )
+            if ( min_dist_sq_( a_idx, b_idx ) > radius2_ )
                 return;
 
             const auto& a_node = nodes_[a_idx];
