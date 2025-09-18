b790561ce0ab1fbc79d45b32dc82e262da149792
Matteo Ceccarello <matteo.ceccarello@unipd.it>
2025-09-18 14:09:09 +0200

    add structured logging
    We now have a panna/logging.hpp header that allows for simple structured
    logging.
    
    The outputs thus produced can be piped into `hl` (https://github.com/pamburus/hl),
    which has been added to the flake.

--- flake.nix
+++ flake.nix
@@ -2,46 +2,55 @@
   description = "PUFFINN - Parameterless and Universal Fast FInding of Nearest Neighbors";
 
   inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+  inputs.hl.url = "github:pamburus/hl";
 
   outputs = {
     self,
     nixpkgs,
+    hl,
   }: let
     supportedSystems = ["x86_64-linux" "aarch64-linux" "x86_64-darwin" "aarch64-darwin"];
     forEachSupportedSystem = f:
       nixpkgs.lib.genAttrs supportedSystems (system:
         f {
           pkgs = import nixpkgs {inherit system;};
+          hlbin = hl.packages.${system}.default;
         });
   in {
-    devShells = forEachSupportedSystem ({pkgs}: {
-      default =(pkgs.mkShell.override { stdenv = pkgs.clangStdenv; }) {
+    devShells = forEachSupportedSystem ({
+      pkgs,
+      hlbin,
+    }: {
+      default = (pkgs.mkShell.override {stdenv = pkgs.clangStdenv;}) {
         venvDir = ".venv";
-        
-        packages = with pkgs; [
-          clang-tools
-          clang-analyzer
-          lldb
-          python312
-          sqlite-interactive
-          cmake
-          just
-          bear # To generate compile_commands.json files
-          llvmPackages.openmp
-          llvmPackages.libcxx
-          rr
-          gdbgui
-          valgrind
-          highfive
-          samply
-        ] ++ ( with python312Packages; [
-          venvShellHook
-          numpy
-          h5py
-          nanobind
-          icecream
-          scikit-build-core
-        ]);
+
+        packages = with pkgs;
+          [
+            clang-tools
+            clang-analyzer
+            lldb
+            python312
+            sqlite-interactive
+            cmake
+            just
+            bear # To generate compile_commands.json files
+            llvmPackages.openmp
+            llvmPackages.libcxx
+            rr
+            gdbgui
+            valgrind
+            highfive
+            samply
+          ]
+          ++ (with pkgs.python312Packages; [
+            venvShellHook
+            numpy
+            h5py
+            nanobind
+            icecream
+            scikit-build-core
+          ])
+          ++ [hlbin];
 
         NIX_ENFORCE_NO_NATIVE = false;
       };
