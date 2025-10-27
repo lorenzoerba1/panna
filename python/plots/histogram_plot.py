40f5dffb595165d16066b1311cdb572158294180
francescopio.monaco@studenti.unipd.it <francescopio.monaco@studenti.unipd.it>
2025-10-27 14:03:12 +0100

    Push things

--- python/plots/histogram_plot.py
+++ python/plots/histogram_plot.py
@@ -12,9 +12,22 @@ if __name__ == "__main__":
     filepath_prefix = Path(__file__).resolve().parents[2]
     filepath = os.path.join(filepath_prefix, "histogram.csv")
     df = pd.read_csv(filepath)
-    sns.set_theme(palette="muted", style="white")
+    # Drop the last row
+    df = df[:-1]
+    df = df[1:]
+    # For the rugplot we have to replicate the weights according to their counts
+    expanded_weights = np.repeat(df["weight"], df["count"])
+    expanded_df = pd.DataFrame({"weight": expanded_weights})
+
+    sns.set_theme(palette="muted", style="white" )
     
-    fig, axs = plt.subplots(figsize=(10, 6), nrows=1, ncols=1)
-    sns.barplot(data=df, x="weight", y="count", ax=axs)
+    fig, axs = plt.subplots(figsize=(10, 6), nrows=1, ncols=1, layout="constrained")
+    sns.histplot(data=expanded_df, x="weight", ax=axs)
+    
+    sns.rugplot(data=df, x="weight", ax=axs, height=-0.02,clip_on=False)
+    axs.set_xlabel("Edge Weight")
+    axs.set_ylabel("Count")
+    sns.despine(left=True, bottom=True)
+    #axs.set_xscale("log")
     
     plt.show()
