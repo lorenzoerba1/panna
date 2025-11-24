849d6886af70eddae6b96cdcb1f8f59bdc17fef7
francescopio.monaco@studenti.unipd.it <francescopio.monaco@studenti.unipd.it>
2025-11-24 08:54:04 +0100

    Update flake

--- flake.nix
+++ flake.nix
@@ -82,27 +82,50 @@
             cp -r include/* $out/include
           '';
         };
-        # TODO install the damn thing from source
-        # tree-similarity = pkgs.stdenv.mkDerivation rec {
-        #   name = "tree-similarity";
-        #   version = "0.1.1";
-        #   src = pkgs.fetchFromGitHub {
-        #     owner = "DatabaseGroup";
-        #     repo = "tree-similarity";
-        #     rev = "v${version}";
-        #     hash = "sha256-f20ad2ffb2852ebee6033775cdaa369e0cf4b7fd";
-        #   };
-        #   buildInputs = with pkgs; [
-        #     cmake
-        #     ninja
-        #   ];
-        #   installPhase = ''
-        #     mkdir $out/build
-        #     cd $out/build
-        #     cmake ..
-        #     ninja install
-        #   '';
-        # };
+
+    # tree-similarity = pkgs.stdenv.mkDerivation rec {
+    #   pname = "tree-similarity";
+    #   version = "0.1.1";
+
+    #   src = pkgs.fetchFromGitHub {
+    #     owner = "DatabaseGroup";
+    #     repo = "tree-similarity";
+    #     rev = "059b0bc1b06d683fa77f3c1df310688190c6ea75";
+    #     hash = "sha256-xTm8wA05nH19x/WfOrgO4VKkfm3vEwob0H/6WTsvJh4=";
+    #   };
+
+
+    #   nativeBuildInputs = [
+    #     pkgs.cmake
+    #     pkgs.ninja
+    #     pkgs.pkg-config
+    #   ];
+
+    #   buildInputs = [
+    #     pkgs.llvmPackages.libcxx
+    #   ];
+
+    #   cmakeFlags = [
+    #     "-DCMAKE_BUILD_TYPE=Release"
+    #     "-GNinja"
+    #   ];
+
+    #   configurePhase = ''
+    #     mkdir build
+    #     cd build
+    #     cmake ${src} $cmakeFlags
+    #   '';
+
+    #   buildPhase = ''
+    #     cd build
+    #     ninja
+    #   '';
+
+    #   installPhase = ''
+    #     cd build
+    #     ninja install
+    #   '';
+    # };
 
         panna-python = python.pkgs.buildPythonPackage {
           pname = "panna";
@@ -217,7 +240,7 @@
             mlpack
             armadillo
             hl-bin
-            tree-similarity
+            # tree-similarity
           ];
 
           NIX_ENFORCE_NO_NATIVE = false;
